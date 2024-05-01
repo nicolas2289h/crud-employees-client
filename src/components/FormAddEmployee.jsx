6a83dcfd466c8f386cdf8d7cb2d5fd349b92cca7
Nico <nicolas2289h@gmail.com>
2024-05-01 19:19:53 -0300

    modificando FormAddEmployee para agregar foto de perfil

--- src/components/FormAddEmployee.jsx
+++ src/components/FormAddEmployee.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import NoUserImage from '../assets/images/no-user.jpg'
 
 import ModalNotification from './ModalNotification'
 
